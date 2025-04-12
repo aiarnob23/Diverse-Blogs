61fb16b8ca2c5ef12eed366a67f7f629329386cb
aiarnob23 <aiarnob23@gmail.com>
2025-04-12 11:53:12 +0600

    firebase updated

--- frontend/src/firebase/firebase.config.ts
+++ frontend/src/firebase/firebase.config.ts
@@ -1,15 +1,14 @@
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDvGopF_8bepgYHEurTJljoFKaajMhuCQ8",
-  authDomain: "diverse-blogs-67a97.firebaseapp.com",
-  projectId: "diverse-blogs-67a97",
-  storageBucket: "diverse-blogs-67a97.firebasestorage.app",
-  messagingSenderId: "364952013844",
-  appId: "1:364952013844:web:ecf4f16303e05e3a00eb01",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
