8c3f1c028b2ad17845c7b08bc79c8b3de127dabd
aiarnob23 <aiarnob23@gmail.com>
2025-04-12 11:45:09 +0600

    deployment done

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,10 +12,12 @@ const app: Application = express();
 //cors and middlewares
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials:true,
-}));
+app.use(
+  cors({
+    origin: "https://diverse-blogs-frontend.vercel.app",
+    credentials: true,
+  })
+);
 
 //routes
 app.use("/api/auth", AuthRoutes);
